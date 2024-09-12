ae40755c2938a0ce7ef10e8dcfd7726d6cd9f86d
Alan Carlos <alancarlospereira.ti@gmail.com>
2024-09-12 00:10:30 -0300

    testando upload de imagem

---
+++ src/Ingenico.Barcode.Data/Migrations/20240912020643_MigrationImages.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ingenico.Barcode.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Produto",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Produto");
+        }
+    }
+}
---
+++ src/Ingenico.Barcode.Data/Repositorios/ImagemUploadService.cs
@@ -0,0 +1,28 @@
+﻿using Ingenico.Barcode.Domain.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Ingenico.Barcode.Data.Repositorios;
+
+public class ImageUploadService : IImageUploadService {
+    private readonly string _imageFolderPath;
+
+    public ImageUploadService(IConfiguration configuration) {
+        // Lê o caminho do appsettings.json
+        _imageFolderPath = configuration["ImageSettings:ImageFolderPath"];
+    }
+
+    public string UploadImage(IFormFile image) {
+        if (image == null || image.Length == 0)
+            throw new ArgumentException("Imagem inválida.");
+
+        var imageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+        var imagePath = Path.Combine(_imageFolderPath, imageName);
+
+        using (var stream = new FileStream(imagePath, FileMode.Create)) {
+            image.CopyTo(stream);
+        }
+
+        return imageName; // Retorna o nome do arquivo para salvar no banco
+    }
+}
